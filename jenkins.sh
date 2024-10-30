61ca09e36bf0e6d2193ff0cce59f9a9d8e852031
Mohammad Sameer <141007601+SameerMohammad-45@users.noreply.github.com>
2024-10-30 20:49:46 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,5 +1,5 @@
 
-yum install git java-1.8.0-openjdk maven -y
+yum install java-17-amazon-corretto-jmods -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 amazon-linux-extras install java-openjdk11 -y
